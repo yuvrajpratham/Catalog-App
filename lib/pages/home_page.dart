75e98481472d88e5bc3f130a5c7807067d0dd76a
Yuvraj Pratham <yuvrajpratham01@gmail.com>
2023-06-30 19:32:37 +0530

    day16 ui-ux completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,6 +4,10 @@ import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/widgets/drawer.dart';
 import '../widgets/item_widgets.dart';
+import 'package:velocity_x/velocity_x.dart';
+
+import '../widgets/themes.dart';
+
 
 
 class HomePage extends StatefulWidget {
@@ -40,22 +44,103 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Catalog App" ,style: TextStyle(color: Colors.black),),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items!=null&&CatalogModel.items.isNotEmpty)?  ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidget(
-              item: CatalogModel.items[index],
-            );
-          },
-        ):Center(
-          child:CircularProgressIndicator(),),
-      ),
-      drawer: MyDrawer(),
+      backgroundColor: MyTheme.creamColor,
+     
+      body: SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+             CatalogHeader(),
+             if(CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+             CatalogList().expand()
+                else
+                  Center(
+                    child: CircularProgressIndicator(),
+                  )
+            ],
+            ),
+        ),
+      )
     );
+  }
+}
+class CatalogHeader extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              "Catalog App".text.xl5.bold.color(MyTheme.darkBluishColor).make(),
+              "Trendingproducts".text.xl2.make(),
+            ],
+            );
+}
+}
+class CatalogList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+       shrinkWrap: true,
+      itemCount: CatalogModel.items.length,
+      itemBuilder: (context, index) {
+        final catalog = CatalogModel.items[index];
+        return CatalogItem(catalog: catalog);
+      },
+    );
+  }
+}
+class CatalogItem extends StatelessWidget {
+  final Item catalog;
+
+  const CatalogItem({
+    required this.catalog,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return VxBox(
+      child:Row(
+        children: [
+        CatalogImage(image:catalog.image,),
+        Expanded(child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          catalog.name.text.lg.color(MyTheme.darkBluishColor).bold.make(),
+          catalog.desc.text.textStyle(context.captionStyle).make(),
+
+10.heightBox,
+
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceBetween,
+            buttonPadding: EdgeInsets.zero,
+            children: [
+              "\$${catalog.price}".text.bold.xl.make(),
+              ElevatedButton(onPressed: (){},
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(MyTheme.darkBluishColor),
+                shape: MaterialStateProperty.all(StadiumBorder(),)
+              ),
+               child: "Buy".text.color(Colors.white).make(),)
+
+          ],).pOnly(right: 8.0)
+        ],
+
+        ))
+        ],)
+      ).white.rounded.square(150).make().py16();
+  }
+}
+class CatalogImage extends StatelessWidget {
+final String image;
+
+  const CatalogImage({super.key, required this.image});
+  @override
+  Widget build(BuildContext context) {
+    return   Image.network(image).box.rounded.p8.color(MyTheme.creamColor).make().p16().w40(context);
   }
 }
